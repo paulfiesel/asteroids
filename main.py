54a5d2dbb6a96069bc910caa9e98bc3c8c08c588
Paul Fiesel <paul.fiesel@gmail.com>
2024-12-06 18:45:09 -0600

    feat/ Added framerate limit

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def main():
 
     # Initialize Pygame
     pygame.init()
-    # Create a screen object
+    # Create a screen object, clock
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     #Main game loop
     while True:
@@ -23,7 +24,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-        
+       #Limit the framerate
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
