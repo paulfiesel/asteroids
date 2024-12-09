eb8d12cf8e1e4906ffd92b722949086a146aa5be
Paul Fiesel <paul.fiesel@gmail.com>
2024-12-08 18:31:06 -0600

    chore/ Updated files, fixed conflict

--- main [conflicted].py
+++ main [conflicted].py
@@ -1,36 +1,29 @@
-# this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-    # Initialize Pygame
     pygame.init()
-    # Create a screen object, clock
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    # Create a Player object
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-    #Main game loop
     while True:
-        #Quit Pygame with "X"
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         player.update(dt)
 
-        # Color, draw, then update the display
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
-       #Limit the framerate
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
-        
 
 if __name__ == "__main__":
     main()
